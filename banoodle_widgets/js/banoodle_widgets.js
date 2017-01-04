d54ca0d1616014bac2fddb4a6b7803e14de54b9d
annebonham <abonham@yahoo.com>
2017-01-04 10:48:14 -0800

    Create banoodle_widgets.js

---
+++ banoodle_widgets/js/banoodle_widgets.js
@@ -0,0 +1,12 @@
+(function($) {
+	$(document).ready(function(){
+    // Apply jQuery UI Accordion behaviors to specifically classed elements.
+    $('.accordion').accordion({
+      header: ".accordion__title",
+      collapsible: true,
+      active: false,
+      animated: false
+    });
+
+	});
+}(jQuery));
